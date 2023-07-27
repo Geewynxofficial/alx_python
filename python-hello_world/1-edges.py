a84186efd832a679ca59c2c00fb12526274c8418
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-27 02:32:25 +0100

    school

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-word = "School"
-word_first_3 = word[: -3]
+word = "Holberton"
+word_first_3 = word[: -6]
 print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word[-2:]
 print("Last 2 letters: {}".format(word_last_2))
