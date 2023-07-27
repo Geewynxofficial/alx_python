b6f927ba4fd4caf4ca084b63c4fa50f1e96b3200
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-27 12:20:05 +0100

    correct

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[: -6]
-print("First 3 letters: {}".format(word_first_3))
-word_last_2 = word[-2:]
-print("Last 2 letters: {}".format(word_last_2))
-middle_word = word[1: -1]
-print("Middle word: {}".format(middle_word))
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1:-1]))
