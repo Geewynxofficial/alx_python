da5ccc4d41b968eb088abea1177832ab8088cf6b
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-27 02:37:51 +0100

    schol

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[: -6]
-print("First 3 letters: {}".format(word_first_3))
+print("First 3 letters: {}".format(Sch))
 word_last_2 = word[-2:]
-print("Last 2 letters: {}".format(word_last_2))
+print("Last 2 letters: {}".format(ol))
 middle_word = word[1: -1]
-print("Middle word: {}".format(middle_word))
+print("Middle word: {}".format(choo))
