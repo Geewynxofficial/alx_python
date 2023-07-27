b3c50090013f99fa25780eaaf32950eb8bc6d747
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-27 11:59:06 +0100

    in

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 for i in range(0,10):
-    for j in range(i+1,10):
-        print(" {:01d}{:01d}".format(i, j), end="," if i < 8 else "\n")
+    for j in range(i+1, 10):
+        print("{:01d}{:01d}".format(i, j), end=", " if i < 8 else "\n")
 print()
