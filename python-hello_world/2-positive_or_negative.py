bbabd315502193af54e1bf4af3eb94ebbe2168ff
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-27 02:01:29 +0100

    negapotive

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number > 0:
+    print(number,"is positive.")
+elif number == 0:
+    print(number,"is zero.")
+else:
+    print(number,"is negative.")
+if -4 < 0:
+    print("-4 is negative")
+if 0 == 0:
+    print("0 is zero")
+if -3 < 0: 
+    print("-3 is negative")
+if -10 < 0:
+    print("-10 is negative")
+if 10 > 0:
+    print("10 is positive")
+if -5 < 0:
+    print("-5 is negative")
+if 6 > 0:
+    print("6 is positive")
+if 7 > 0:
+    print("7 is positive")
+if 5 > 0:
+    print("5 is positive")
+    
+
+
+
+
+
+    
---
+++ python-hello_world/text.py
@@ -0,0 +1,4 @@
+#This is a comment.
+print("Hello, World!")
+if 5 > 2:
+    print("Five is greater than two!") 
