df7a8f2d62745ae7a803b8f0ab29ac0e7041ae75
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-28 20:16:59 +0100

    dddd

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-pow = __import__('1-main').pow
+#!/usr/bin/python3
+
 def pow(a, b):
-    return a ^ b
+    if b == 0:
+        return 1
+
+    result = 1
+    base = a
+    exponent = abs(b)
+
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result *= base
+        base *= base
+        exponent //= 2
+
+    return result if b >= 0 else 1 / result
