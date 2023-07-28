bbdb0e2a9509cc6eb344490394005274a6873387
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-28 06:42:50 +0100

    update

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -9,21 +9,21 @@ elif number < 0:
     print(number,"is negative.")
 else:
     print(number,"is zero.")
-if -4 < 0:
-    print("-4 is negative")
-if 0 == 0:
-    print("0 is zero")
-if -3 < 0: 
-    print("-3 is negative")
-if -10 < 0:
-    print("-10 is negative")
-if 10 > 0:
-    print("10 is positive")
-if -5 < 0:
-    print("-5 is negative")
-if 6 > 0:
-    print("6 is positive")
-if 7 > 0:
-    print("7 is positive")
-if 5 > 0:
-    print("5 is positive")
+-4 < 0
+print("-4 is negative")
+0 == 0
+print("0 is zero")
+-3 < 0
+print("-3 is negative")
+-10 < 0
+print("-10 is negative")
+10 > 0
+print("10 is positive")
+-5 < 0
+print("-5 is negative")
+6 > 0
+print("6 is positive")
+7 > 0
+print("7 is positive")
+5 > 0
+print("5 is positive")
