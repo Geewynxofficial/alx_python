e70075106e040f941fc21e4334335a3ce6fb7984
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-29 14:33:43 +0100

    add

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+import importlib
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+add_module = importlib.import_module('add_0')
+add = add_module.add
+
+# Perform the addition and print the result
+result = add(a, b)
+print(f"{a} + {b} = {result}")
