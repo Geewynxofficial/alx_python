3bfd6b22f3646a7e481548994b073f22ed923ca9
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-29 06:15:57 +0100

    temp

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
     return celsius
-"""Converts a temperature in Fahrenheit to Celsius."""
-celsius = (fahrenheit - 32) * 5 / 9
