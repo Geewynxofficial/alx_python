f453edcdf506631d14591e4fd39b7089b5053641
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-31 00:55:20 +0100

    argument

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+    # Get the number of arguments
+    num_args = len(sys.argv) - 1  # The first element in sys.argv is the script name itself
+
+    # Print the number of arguments
+    print(f"Number of argument(s): {num_args}")
+
+    if num_args > 0:
+        # Print the list of arguments
+        print(f"Arguments:")
+
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}: {arg}")
+    else:
+        # No arguments were passed
+        print(":")
