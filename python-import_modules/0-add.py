36112d28d09145163eae30ecdc4884be1eb6e1ff
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-07-30 01:20:36 +0100

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,2 +1,11 @@
-def add(a, b):
-    return a + b
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+add_module = __import__("add_0")
+add = add_module.add
+
+# Print the result of the addition
+result = add(a, b)
+print(f"{a} + {b} = {result}")
