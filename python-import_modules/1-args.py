567803ff92a1e95db30dddbfbb602cb739a16e03
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-01 03:36:02 +0100

    argu

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     if num_args > 0:
         # Print the list of arguments
-        print("Arguments:")
+        print("{} arguments:")
 
         for i, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, arg))
