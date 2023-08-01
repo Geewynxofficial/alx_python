b97e76641ea40a8a92729dc1ab4b1524f26e513c
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-01 10:39:51 +0100

    print

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,4 +1,12 @@
-from variable_load_2 import a
+def load_variable():
+    with open("variable_load_2.py", "r") as f:
+        source = f.read()
+
+    # Import the variable a from the source code
+    a = eval(source)["a"]
+
+    return a
+
+
 if __name__ == "__main__":
-    a = 98
-    print(a)
+    print(load_variable())
