03cba99052e53a631e97218ce5dcdce1e77fef81
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-01 04:37:59 +0100

    win

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,1 +1,11 @@
 def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Division by zero is not allowed.")
+        return None
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        print("Finally block executed.")
