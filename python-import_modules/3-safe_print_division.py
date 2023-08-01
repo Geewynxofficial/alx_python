0255209fe313e5b509834ded4f70d2a68bfbf065
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-01 10:34:39 +0100

    win

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,9 +2,14 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Division by zero is not allowed.")
+        # print("Division by zero is not allowed.")
         result = None
-    else:
-        print("Inside result: {}".format(result))
     finally:
-        print("{} / {} = {}")
+        print("Inside result: {}".format(result))
+    return result
+if __name__ == "__main__":
+  result = safe_print_division(10, 2)
+  print(result)
+
+  result = safe_print_division(10, 0)
+  print(result)
