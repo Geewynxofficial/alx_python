66824d1bd402c57b05ffd891547d29a23da2ebd7
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-01 06:05:01 +0100

    int

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,6 +6,5 @@ def safe_print_division(a, b):
         return None
     else:
         print("Inside result: {}".format(result))
-        return result
     finally:
-        print()
+        return result
