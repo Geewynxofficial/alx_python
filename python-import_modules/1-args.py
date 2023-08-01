093b0d80a6e26a06dae92fe2a03648e916efba6f
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-01 20:56:33 +0100

    argu

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,19 +1,35 @@
+# import sys
+
+# if __name__ == "__main__":
+#     # Get the number of arguments
+#     num_args = len(sys.argv) - 1  # The first element in sys.argv is the script name itself
+
+#     # Print the number of arguments
+#     # print("{} argument:".format(num_args))
+
+#     if num_args > 0:
+#         # Print the list of arguments
+#         print("{} arguments:".format(num_args))
+
+#         for i, arg in enumerate(sys.argv[1:], start=1):
+#             print("{}: {}".format(i, arg))
+#     else:
+#         # No arguments were passed
+#         print("{} arguments.".format(num_args))
+
+
+
 import sys
 
-if __name__ == "__main__":
-    # Get the number of arguments
-    num_args = len(sys.argv) - 1  # The first element in sys.argv is the script name itself
+def print_arguments():
+    argv = sys.argv[1:]
+    num_args = len(argv)
 
-    # Print the number of arguments
-    # print("{} argument:".format(num_args))
+    print(f"{num_args} argument{'s' if num_args != 1 else ''}{':' if num_args > 0 else '.'}")
 
     if num_args > 0:
-        # Print the list of arguments
-        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(argv, 1):
+            print(f"{i}: {arg}")
 
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
-    else:
-        # No arguments were passed
-        print("{} arguments.".format(num_args))
-
+if _name_ == "_main_":
+    print_arguments()
