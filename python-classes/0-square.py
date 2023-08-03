1bb3457c8eddba9824ec33408ccb6387187d6264
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-03 23:40:24 +0100

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,26 +1,16 @@
 class Square:
-    """
-    A class that defines a square by its size.
-
-    Attributes:
-        size (int): The size of the square.
-    """
-
     def __init__(self, size):
-        """
-        Initialize a new square.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
 
-    def __repr__(self):
-        """
-        Return a string representation of the square.
+    def area(self):
+        return self.__size ** 2
 
-        Returns:
-            str: A string representation of the square.
-        """
-        return f"Square({self.__size})"
+    def perimeter(self):
+        return 4 * self.__size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, new_size):
+        self.__size = new_size
 
