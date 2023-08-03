1fcf01c8cf31444ae78e68b17d82c21af4dadd46
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-03 21:44:38 +0100

    square

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+        @property
+        def size(self):
+            return self.__size
+        square = Square(3)
+        print(square.size)
