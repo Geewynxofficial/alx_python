1aa6957e725a47e29e079e48ff977e7268449830
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-06 09:26:33 +0100

    ggggg

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+"""
+This module defines a class for representing squares.
+
+The `Square` class has a single attribute, `size`, which represents the size of the square. The `Square` class also has a `__repr__()` method, which returns a string representation of the square.
+"""
+class Square:
+    """A class representing squares."""
+
+    def __init__(self, size):
+        """Initialize a square with the given size."""
+        self.size = size
+
+    def __repr__(self):
+        """Return a string representation of the square."""
+        return f"Square(size={self.size})"
+
+
+square = Square(5)
+print(square)
