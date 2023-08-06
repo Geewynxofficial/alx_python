aa6b61bf66ad28a1f9affb95ca812f96eee4a501
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-06 09:20:27 +0100

    size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,8 @@
+"""
+This module defines a class for representing squares.
+
+The `Square` class has a single attribute, `size`, which represents the size of the square. The `Square` class also has a `__repr__()` method, which returns a string representation of the square.
+"""
 class Square:
     """
     A class that defines a square by its size.
