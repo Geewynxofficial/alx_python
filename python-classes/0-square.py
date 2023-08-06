d8bfb5c38e2f63cb0d634c6594e0ba725a973c92
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-06 09:11:45 +0100

    coeer

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,20 +7,22 @@ class Square:
     """
 
     def __init__(self, size):
+        """ Initialize a new square. """ 
+        
         """
-        Initialize a new square.
-
         Args:
-            size (int): The size of the square.
+        size (int): The size of the square.
         """
         self.__size = size
 
     def __repr__(self):
         """
         Return a string representation of the square.
+        """
 
+        """
         Returns:
-            str: A string representation of the square.
+        str: A string representation of the square.
         """
         return f"Square({self.__size})"
 
