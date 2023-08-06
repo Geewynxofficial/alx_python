d452b2419f43442fbd0df5855fd5e097feceac2d
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-06 09:43:30 +0100

    ffggff

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,6 +4,8 @@ This module defines a class for representing squares.
 The `Square` class has a single attribute, `size`, which represents the size of the square. The `Square` class also has a `__repr__()` method, which returns a string representation of the square.
 """
 class Square:
+    """A square with a side length of `size`."""
+
 
     # Private instance attribute
     _size = 0
