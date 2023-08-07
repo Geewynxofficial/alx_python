5c34f844edbc1e86439d1fe19a4ba4d6a2ee62c8
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-08 00:50:33 +0100

    inherit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,7 +2,7 @@
 def inherits_from(obj, a_class):
   """Returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False."""
   
-  if isinstance(obj, a_class):
+  if not isinstance(obj, a_class):
     return True
 
   # Check if the object's class is a subclass of the specified class.
