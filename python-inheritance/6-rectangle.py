c926a5450bdeb4f9e7381ce051c47c4bac3ab488
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 23:28:39 +0100

    gsggsg

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,28 @@
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """ geo """
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        descript method
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        else:
+            return value
+class Rectangle(BaseGeometry):
+    """rectangle"""
+
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+        
+
