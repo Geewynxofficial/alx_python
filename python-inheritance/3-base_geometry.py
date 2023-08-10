4239b41fb12b02ea0d2c70b7606ccc57381d723e
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 19:35:15 +0100

    hhhehe

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,10 @@
 """basegeometry"""
+
 class BaseGeometry:
-    """
-    base
-    """
-    def __dir__(self):
-        attributes = super().__dir__()
-        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
-        return new_attribute_list
-    
-a = BaseGeometry()
+    """ base """
+    pass
+    # def __dir__(self):
+    #     attributes = super().__dir__()
+    #     new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+    #     return new_attribute_list
+
