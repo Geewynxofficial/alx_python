ca91d940d1c087aa97e65f1ec9dbe7d6ef5495bc
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 21:00:44 +0100

    ggggd

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,12 +1,24 @@
 """basegeometry"""
+from collections.abc import Iterable
 
-class BaseGeometry(metaclass= type):
-    """ base """
 
-def __dir__(self):
-    attributes = super.__dir__()
-    new_attribute_list = [item for item in attributes if item !=
-    "__init_subclass__"]
-    return new_attribute_list
+class BaseGeometryMeta(type):
+    """ type class """
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [
+            item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+class BaseGeometry(metaclass=BaseGeometryMeta):
+
+    """ description base """
+    
+    def __dir__(self):
+        attributes = super.__dir__()
+        new_attribute_list = [
+            item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    
+a = BaseGeometry()
     
 a = BaseGeometry()
