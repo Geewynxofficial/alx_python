0de54b08e8f4a98a8a149c5e1a4b369fd7b677b3
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 22:33:37 +0100

    djdjdjdj

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,7 +1,6 @@
 """basegeometry"""
 class BaseGeometry(metaclass=BaseGeometryMeta):
-
-      """ geo """
-      def area(self):
-          raise Exception("area() is not implemented")
+    """ geo """
+    def area(self):
+        raise Exception("area() is not implemented")
 a = BaseGeometry()
