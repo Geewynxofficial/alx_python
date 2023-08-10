235e6b87f3d877e978c73293fb4e7ded6ca497f8
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-11 00:15:14 +0100

    fitn

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,16 @@
+def print_matrix_integer(matrix=[[]]):
+  """Prints a matrix of integers.
+
+  Args:
+    matrix: The matrix to be printed.
+  """
+
+  if not matrix:
+    print("")
+    return
+
+  for row in matrix:
+    for col in row:
+      print(f"{col:5d}", end=" ")
+    print("")
+
