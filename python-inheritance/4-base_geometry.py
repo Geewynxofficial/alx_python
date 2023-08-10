f6d39ac7365bbc5c99c0748c56c8b90f653ee720
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 22:25:17 +0100

    gsgssg

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,23 +1,5 @@
 """basegeometry"""
-class BaseGeometryMeta(type):
-    """ type class """
-    def __dir__(self):
-        attributes = super().__dir__()
-        new_attribute_list = [
-            item for item in attributes if item != "__init_subclass__"]
-        return new_attribute_list
-    
-class BaseGeometry(metaclass=BaseGeometryMeta):
+class BaseGeometry:
 
-    """ description base """
-    
-    def __dir__(self):
-        attributes = super().__dir__()
-        new_attribute_list = [
-            item for item in attributes if item != "__init_subclass__"]
-        return new_attribute_list
-    
-def area(self):
-    raise Exception("area() is not implemented")
-
-a = BaseGeometry()
+    def area(self):
+        raise Exception("area() is not implemented")
