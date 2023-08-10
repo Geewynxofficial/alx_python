92bb321f265d1ebce1f301ae3a064aeb7902a562
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 21:08:28 +0100

    base

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry(metaclass=BaseGeometryMeta):
     """ description base """
     
     def __dir__(self):
-        attributes = super.__dir__()
+        attributes = super().__dir__()
         new_attribute_list = [
             item for item in attributes if item != "__init_subclass__"]
         return new_attribute_list
