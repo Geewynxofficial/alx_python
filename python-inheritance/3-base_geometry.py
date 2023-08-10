3e3f93fefaebd42263e3bd63a6f9179a56465abb
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-10 21:05:13 +0100

    gdgdgdg

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,6 +6,7 @@ class BaseGeometryMeta(type):
         new_attribute_list = [
             item for item in attributes if item != "__init_subclass__"]
         return new_attribute_list
+    
 class BaseGeometry(metaclass=BaseGeometryMeta):
 
     """ description base """
