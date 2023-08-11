230a01db266e2d79e73430c01242fa6fd78bf0f1
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-11 01:28:35 +0100

    hdgdgdgdg

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        print(" ".join([str(col).rjust(5) for col in row]))
         for col in row:
             print("{:d}".format(col), end=" " if col != row[-1] else "")
             print("")
