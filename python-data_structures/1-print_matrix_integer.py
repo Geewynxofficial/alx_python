89a53c8264575577daa88545881e617ae771103e
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-11 17:25:16 +0100

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
-
-  for row in matrix:
-    for col in row:
-       
-       print("{:d}".format(col), end=" " if col != row[-1] else "")
-       print()
+    
+    for row in matrix:
+        
+        for num in row:
+            print("{:d}".format(num), end="")
+            print()
 
 
 
