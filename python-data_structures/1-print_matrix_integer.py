f00de790372589b2cfe7106220151a3fbd3bdd40
Godwin <133351706+Geewynxofficial@users.noreply.github.com>
2023-08-11 01:22:35 +0100

    hhhggh

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,13 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
-  """Prints a matrix of integers.
-
-  Args:
-    matrix: The matrix to be printed.
-  """
-  for row in matrix:
-    for col in row:
-       print("{:d}".format(col), end=" " if col != row[-1] else "")
-       print("")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+            print("")
 
 
 
