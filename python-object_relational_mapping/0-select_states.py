a74f6cc9b4535b882e3ee3e8e6fcbf8aefa09031
Geewynxofficial <godwinning247@gmail.com>
2023-08-29 11:16:09 +0100

    system

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,18 @@
 import MySQLdb
+import sys
 
-database = MySQLdb.connect(host="localhost", port = 3306, user="Godwin", passwd="Geezee3200_", db="hbtn_0e_0_usa")
-
-cursor = database.cursor()
-cursor.execute ("SELECT * FROM states ORDER BY id ASC")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-cursor.close()
-database.close()
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+    host="localhost", 
+    port = 3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3])
+    
+    cursor = database.cursor()
+    cursor.execute ("SELECT * FROM states.id ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    database.close()
