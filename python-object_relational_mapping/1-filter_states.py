99ea9194c78e6145ee0ccf96020049de2703d3f8
Geewynxofficial <godwinning247@gmail.com>
2023-08-31 18:57:24 +0100

    query

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,20 +3,28 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3]
-    )
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    try:
+        database = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3]
+        )
+    except MySQLdb.Error as e:
+        print(f"Error connecting to the database: {e}")
+        sys.exit(1)
 
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
-        print(rows)
+        print(row)
 
     cursor.close()
     database.close()
