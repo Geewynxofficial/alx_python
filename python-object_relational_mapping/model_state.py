770b86964fa0a9f0ee6cb9981bc1c9d0f6702be7
Geewynxofficial <godwinning247@gmail.com>
2023-09-28 04:05:59 +0100

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
-#!/usr/bin/python3
+Base = declarative_base()
 
-import sys
-from model_state import Base, State
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
-from sqlalchemy import (create_engine)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    State.metadata.create_all(engine)
+engine = create_engine('mysql://username:password@localhost:3306/database_name')
+Base.metadata.create_all(engine)
